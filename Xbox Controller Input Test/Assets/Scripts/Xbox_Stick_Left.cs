9d277ce9163aec2fa27c5edf7263bfe014208de0
Patrick McCaffrey <allknowingq@gmail.com>
2019-01-09 04:04:27 -0500

    Surface - Xbox Controller Input Test - Failed attempt at global LStick script

--- Xbox Controller Input Test/Assets/Scripts/Xbox_Stick_Left.cs
+++ Xbox Controller Input Test/Assets/Scripts/Xbox_Stick_Left.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Xbox_Stick_Left : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform stickLFaceTrans;
+    private Transform stickLTopTrans;
+
     void Start()
     {
-        
+        stickLFaceTrans = GameObject.Find("XboxOne_Controller_Face/Stick_L").GetComponent<Transform>();
+        stickLTopTrans = GameObject.Find("XboxOne_Controller_Top/Stick_L").GetComponent<Transform>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float StickLHoriz = Input.GetAxis("Horizontal");
+        float StickLVert = Input.GetAxis("Vertical");
+
+        stickLFaceTrans.rotation.Set(StickLVert * 24f + 12f, StickLHoriz * 24f + 12f, 0.0f, 1f);
+        stickLTopTrans.rotation.Set(StickLVert * 24f + 12f, StickLHoriz * 24f + 12f, 0.0f, 1f);
     }
 }
