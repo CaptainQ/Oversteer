cb473d7aff4885340feabb7f660cbaa5d40f84a5
Patrick McCaffrey <allknowingq@gmail.com>
2019-01-10 07:32:12 -0500

    Surface - Xbox Controller Input Test - Added bumpers, DPad, stick clicks, and optimized rotations

--- Xbox Controller Input Test/Assets/Scripts/Xbox_Stick_Left.cs
+++ Xbox Controller Input Test/Assets/Scripts/Xbox_Stick_Left.cs
@@ -15,6 +15,14 @@ public class Xbox_Stick_Left : MonoBehaviour
     public Transform triggerRFaceTrans;
     public Transform triggerRTopTrans;
 
+    public Transform DPadFaceTrans;
+    public Transform DPadTopTrans;
+
+    public Transform bumperLFaceTrans;
+    public Transform bumperLTopTrans;
+    public Transform bumperRFaceTrans;
+    public Transform bumperRTopTrans;
+
     void Start()
     {
         Debug.Log("Start script");
@@ -27,29 +35,40 @@ public class Xbox_Stick_Left : MonoBehaviour
         float stickRHoriz = Input.GetAxis("HorizontalR");
         float stickRVert = Input.GetAxis("VerticalR");
 
+        float stickLClick = Input.GetAxis("ClickL");
+        float stickRClick = Input.GetAxis("ClickR");
+
         float triggerL = Input.GetAxis("TriggerL");
         float triggerR = Input.GetAxis("TriggerR");
 
-        stickLFaceTrans.localRotation = Quaternion.Euler(0, 0, 0);
-        stickLTopTrans.localRotation = Quaternion.Euler(0, 0, 0);
+        float DPadX = Input.GetAxis("DPadX");
+        float DPadY = Input.GetAxis("DPadY");
 
-        stickRFaceTrans.localRotation = Quaternion.Euler(0, 0, 0);
-        stickRTopTrans.localRotation = Quaternion.Euler(0, 0, 0);
+        float bumperL = Input.GetAxis("BumperL");
+        float bumperR = Input.GetAxis("BumperR");
+        
+        stickLFaceTrans.localRotation = Quaternion.Euler(stickLVert * -23f, stickLHoriz * -23f, 0.0f);
+        stickLTopTrans.localRotation = Quaternion.Euler(stickLVert * -23f, stickLHoriz * -23f, 0.0f);
 
-        triggerLFaceTrans.localRotation = Quaternion.Euler(0, 0, 0);
-        triggerLTopTrans.localRotation = Quaternion.Euler(0, 0, 0);
-        triggerRFaceTrans.localRotation = Quaternion.Euler(0, 0, 0);
-        triggerRTopTrans.localRotation = Quaternion.Euler(0, 0, 0);
+        stickRFaceTrans.localRotation = Quaternion.Euler(stickRVert * -23f, stickRHoriz * -23f, 0.0f);
+        stickRTopTrans.localRotation = Quaternion.Euler(stickRVert * -23f, stickRHoriz * -23f, 0.0f);
 
-        stickLFaceTrans.Rotate(stickLVert * -24f, stickLHoriz * -24f, 0.0f);
-        stickLTopTrans.Rotate(stickLVert * -24f, stickLHoriz * -24f, 0.0f);
+        stickLFaceTrans.localPosition = new Vector3(3.384324f, 0.4483004f, 0f - (stickLClick / 8));
+        stickLTopTrans.localPosition = new Vector3(3.384324f, 0.4483004f, 0f - (stickLClick / 8));
+        stickRFaceTrans.localPosition = new Vector3(-1.71292f, -2.186946f, 0f - (stickRClick / 8));
+        stickRTopTrans.localPosition = new Vector3(-1.71292f, -2.186946f, 0f - (stickRClick / 8));
 
-        stickRFaceTrans.Rotate(stickRVert * -24f, stickRHoriz * -24f, 0.0f);
-        stickRTopTrans.Rotate(stickRVert * -24f, stickRHoriz * -24f, 0.0f);
+        triggerLFaceTrans.localRotation = Quaternion.Euler(triggerL * -24f, 0.0f, 0.0f);
+        triggerLTopTrans.localRotation = Quaternion.Euler(triggerL * -24f, 0.0f, 0.0f);
+        triggerRFaceTrans.localRotation = Quaternion.Euler(triggerR * -24f, 0.0f, 0.0f);
+        triggerRTopTrans.localRotation = Quaternion.Euler(triggerR * -24f, 0.0f, 0.0f);
 
-        triggerLFaceTrans.Rotate(triggerL * -24f, 0.0f, 0.0f);
-        triggerLTopTrans.Rotate(triggerL * -24f, 0.0f, 0.0f);
-        triggerRFaceTrans.Rotate(triggerR * -24f, 0.0f, 0.0f);
-        triggerRTopTrans.Rotate(triggerR * -24f, 0.0f, 0.0f);
+        DPadFaceTrans.localRotation = Quaternion.Euler(DPadY * -12, DPadX * -12, 0f);
+        DPadTopTrans.localRotation = Quaternion.Euler(DPadY * -12, DPadX * -12, 0f);
+
+        bumperLFaceTrans.localPosition = new Vector3(0f, 0f - (bumperL / 8));
+        bumperLTopTrans.localPosition = new Vector3(0f, 0f - (bumperL / 8));
+        bumperRFaceTrans.localPosition = new Vector3(0f, 0f - (bumperR / 8));
+        bumperRTopTrans.localPosition = new Vector3(0f, 0f - (bumperR / 8));
     }
 }
